d85faa32d84d1b800b6a208f8c14a944ff871798
MuhireIghor <muhireighor123@gmail.com>
2023-12-18 09:38:51 +0200

    [feat]:Added the math controller

--- src/main/java/com/term1/calculator/controller/MathController.java
+++ src/main/java/com/term1/calculator/controller/MathController.java
@@ -1,4 +1,26 @@
 package com.term1.calculator.controller;
 
-public class DoMath {
+
+
+import com.term1.calculator.dtos.request.DoMathRequestDto;
+import com.term1.calculator.dtos.response.ApiResponse;
+import com.term1.calculator.exceptions.InvalidOperationException;
+import com.term1.calculator.services.impl.MathOperatorImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/calc/make_operation")
+public class MathController {
+    private final MathOperatorImpl mathOperatorImpl;
+
+    public MathController(MathOperatorImpl mathOperatorImpl) {
+        this.mathOperatorImpl = mathOperatorImpl;
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse> doMath(@RequestBody DoMathRequestDto dto) throws InvalidOperationException {
+        return ResponseEntity.ok(ApiResponse.success(mathOperatorImpl.doMath(dto.getOperand1(), dto.getOperand2(), dto.getOperation())));
+    }
 }
